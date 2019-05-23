3c53be5bf9c951ec97830458988e0c97c1b836f4
Alena Choong <alena.choong@intergen.co.nz>
2019-05-24 00:51:57 +1200

    Add video as a background

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
 
@@ -7,6 +6,14 @@ const App: React.FC = () => {
   return (
     <div>
         <Header username="leeeennyy" twitterUrl="https://twitter.com/leeeennyy" youtubeUrl="youtubeyo" emailAddress="myemailaddress" />
+        <div id="background">
+          <div id="background-sheer" />
+          <video className="background-video" loop autoPlay muted>
+            <source src="https://clips-media-assets2.twitch.tv/AT-cm%7C320082885.mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+
     </div>
   );
 }
