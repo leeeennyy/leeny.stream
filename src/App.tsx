8417009f99111d43c8d9c33e3648f854df4682d2
Alena Choong <alena.choong@intergen.co.nz>
2019-05-25 02:44:02 +1200

    A bunch of code to flick between the videos

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header';
+import { Header } from './components/header';
+import { VideoBackgroundPanel } from './components/videoBackgroundPanel';
 
 const App: React.FC = () => {
+  const videoUrls = [
+      "https://clips-media-assets2.twitch.tv/228340281.mp4",
+      "https://clips-media-assets2.twitch.tv/224622285.mp4"
+  ];
+
   return (
     <div>
         <Header username="leeeennyy" twitterUrl="https://twitter.com/leeeennyy" youtubeUrl="youtubeyo" emailAddress="myemailaddress" />
-        <div id="background">
-          <div id="background-sheer" />
-          <video className="background-video" loop autoPlay muted>
-            <source src="https://clips-media-assets2.twitch.tv/AT-cm%7C320082885.mp4" />
-            Your browser does not support the video tag.
-          </video>
-        </div>
-
+        <VideoBackgroundPanel videoUrls={videoUrls}/>
     </div>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,24 +4,51 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 import '../App.css';
 
-interface HeaderProps {
+export interface HeaderProps {
     username: string;
     twitterUrl: string;
     youtubeUrl: string;
     emailAddress: string;
 }
 
-export default class Header extends React.Component<HeaderProps> {
-    public render() {
-        return (
-            <header className="header">
-                <h1>{ this.props.username }</h1>
-                <ul>
-                    <li><a href={ this.props.twitterUrl }><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
-                    <li><a href={ this.props.youtubeUrl }><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
-                    <li><a href={ this.props.emailAddress }><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a></li>
-                </ul>
-            </header>
-        );
-    }
+// export const Header1: React.FunctionComponent<HeaderProps> = props => (
+//     <header className="header">
+//         <h1>{ props.username }</h1>
+//         <ul>
+//             <li><a href={ props.twitterUrl }><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
+//             <li><a href={ props.youtubeUrl }><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
+//             <li><a href={ props.emailAddress }><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a></li>
+//         </ul>
+//     </header>
+// );
+
+export const Header = (headerProps:HeaderProps) => (
+    <header className="header">
+        <h1>{ headerProps.username }</h1>
+        <ul>
+            <li><a href={ headerProps.twitterUrl }><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
+            <li><a href={ headerProps.youtubeUrl }><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
+            <li><a href={ headerProps.emailAddress }><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a></li>
+        </ul>
+    </header>
+);
+
+Header.propTypes = {
+
 }
+
+
+// export default class Header extends React.Component<HeaderProps> {
+//     public render() {
+//         return (
+//             <header className="header">
+//                 <h1>{ this.props.username }</h1>
+//                 <ul>
+//                     <li><a href={ this.props.twitterUrl }><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
+//                     <li><a href={ this.props.youtubeUrl }><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
+//                     <li><a href={ this.props.emailAddress }><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a></li>
+//                 </ul>
+//             </header>
+//         );
+//     }
+// }
