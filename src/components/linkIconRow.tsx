1ec6f9277adff9bcd86bc7585e165b931a9e8f8f
Alena Choong <alena.choong@intergen.co.nz>
2019-05-26 15:30:14 +1200

    Refactored LinkIconRow

--- src/components/linkIconRow.tsx
+++ src/components/linkIconRow.tsx
@@ -5,12 +5,16 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faTwitter, faTwitch, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { IconButton } from './iconButton';
 
+const twitchIcon = <FontAwesomeIcon icon={ faTwitch } />;
+const twitterIcon = <FontAwesomeIcon icon={ faTwitter } />;
+const instagramIcon = <FontAwesomeIcon icon={ faInstagram } />;
+const emailIcon = <FontAwesomeIcon icon={ faEnvelope } />;
 
 export const LinkIconRow = () => (
     <div id="link-icon-row">
-        <IconButton url="https://twitch.tv/leeeennyy" icon={<FontAwesomeIcon icon={faTwitch}/>} openInNewTab />
-        <IconButton url="https://twitter.com/leeeennyy" icon={<FontAwesomeIcon icon={faTwitter}/>} openInNewTab />
-        <IconButton url="https://instagram.com/leeeennyy" icon={<FontAwesomeIcon icon={faInstagram}/>} openInNewTab />
-        <IconButton url="mailto:leeeennyy@gmail.com" icon={<FontAwesomeIcon icon={faEnvelope}/>} />
+        <IconButton url="https://twitch.tv/leeeennyy" icon={ twitchIcon } openInNewTab />
+        <IconButton url="https://twitter.com/leeeennyy" icon={ twitterIcon } openInNewTab />
+        <IconButton url="https://instagram.com/leeeennyy" icon={ instagramIcon } openInNewTab />
+        <IconButton url="mailto:leeeennyy@gmail.com" icon={ emailIcon } />
     </div>
 );
