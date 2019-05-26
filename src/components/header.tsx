cdcdbf734f9243afc37c369f0051fd68d660993a
Alena Choong <alena.choong@intergen.co.nz>
2019-05-26 15:30:00 +1200

    Remove commented out code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -26,29 +26,9 @@ export const Header = (headerProps:HeaderProps) => (
     <header className="header">
         <h1>{ headerProps.username }</h1>
         <ul>
-            <li><a href={ headerProps.twitterUrl }><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
-            <li><a href={ headerProps.youtubeUrl }><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
-            <li><a href={ headerProps.emailAddress }><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a></li>
+            <li><a href={ headerProps.twitterUrl }><FontAwesomeIcon icon={ faTwitter }></FontAwesomeIcon></a></li>
+            <li><a href={ headerProps.youtubeUrl }><FontAwesomeIcon icon={ faYoutube }></FontAwesomeIcon></a></li>
+            <li><a href={ headerProps.emailAddress }><FontAwesomeIcon icon={ faEnvelope }></FontAwesomeIcon></a></li>
         </ul>
     </header>
 );
-
-Header.propTypes = {
-
-}
-
-
-// export default class Header extends React.Component<HeaderProps> {
-//     public render() {
-//         return (
-//             <header className="header">
-//                 <h1>{ this.props.username }</h1>
-//                 <ul>
-//                     <li><a href={ this.props.twitterUrl }><FontAwesomeIcon icon={faTwitter}></FontAwesomeIcon></a></li>
-//                     <li><a href={ this.props.youtubeUrl }><FontAwesomeIcon icon={faYoutube}></FontAwesomeIcon></a></li>
-//                     <li><a href={ this.props.emailAddress }><FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon></a></li>
-//                 </ul>
-//             </header>
-//         );
-//     }
-// }
